3f084f7aff0e710832036df770f3d3cd5a96b3c3
TheBoss000 <Bossmail28@gmail.com>
2022-06-27 05:46:52 +0200

    uploading my first work ever

---
+++ عبدالله بدر الدين على     1 1 1/عبدالله بدر الدين على     1 1 1/Fo1.Designer.vb
@@ -0,0 +1,234 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button
+        Me.Button2 = New System.Windows.Forms.Button
+        Me.Button3 = New System.Windows.Forms.Button
+        Me.Button4 = New System.Windows.Forms.Button
+        Me.Button5 = New System.Windows.Forms.Button
+        Me.L1 = New System.Windows.Forms.Label
+        Me.T1 = New System.Windows.Forms.TextBox
+        Me.T2 = New System.Windows.Forms.TextBox
+        Me.T3 = New System.Windows.Forms.TextBox
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox
+        Me.L2 = New System.Windows.Forms.Label
+        Me.L5 = New System.Windows.Forms.Label
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.Label3 = New System.Windows.Forms.Label
+        Me.Label4 = New System.Windows.Forms.Label
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(176, 200)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(51, 31)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "ضرب"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(5, 200)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(51, 31)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "قسمه"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(62, 200)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(51, 31)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "جمع"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(119, 200)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(51, 31)
+        Me.Button4.TabIndex = 3
+        Me.Button4.Text = "طرح"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(238, 200)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(68, 77)
+        Me.Button5.TabIndex = 4
+        Me.Button5.Text = "خروج"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'L1
+        '
+        Me.L1.AutoSize = True
+        Me.L1.Location = New System.Drawing.Point(49, 110)
+        Me.L1.Name = "L1"
+        Me.L1.Size = New System.Drawing.Size(0, 13)
+        Me.L1.TabIndex = 5
+        '
+        'T1
+        '
+        Me.T1.Location = New System.Drawing.Point(13, 12)
+        Me.T1.Name = "T1"
+        Me.T1.Size = New System.Drawing.Size(100, 20)
+        Me.T1.TabIndex = 6
+        Me.T1.Text = "0"
+        '
+        'T2
+        '
+        Me.T2.Location = New System.Drawing.Point(13, 38)
+        Me.T2.Name = "T2"
+        Me.T2.Size = New System.Drawing.Size(100, 20)
+        Me.T2.TabIndex = 7
+        Me.T2.Text = "0"
+        '
+        'T3
+        '
+        Me.T3.Location = New System.Drawing.Point(13, 64)
+        Me.T3.Name = "T3"
+        Me.T3.Size = New System.Drawing.Size(100, 20)
+        Me.T3.TabIndex = 8
+        Me.T3.Text = "0"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.عبدالله_بدر_الدين_على.My.Resources.Resources._5
+        Me.PictureBox1.Location = New System.Drawing.Point(212, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(104, 111)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage
+        Me.PictureBox1.TabIndex = 9
+        Me.PictureBox1.TabStop = False
+        '
+        'L2
+        '
+        Me.L2.AutoSize = True
+        Me.L2.Location = New System.Drawing.Point(49, 147)
+        Me.L2.Name = "L2"
+        Me.L2.Size = New System.Drawing.Size(0, 13)
+        Me.L2.TabIndex = 10
+        '
+        'L5
+        '
+        Me.L5.AutoSize = True
+        Me.L5.Location = New System.Drawing.Point(197, 138)
+        Me.L5.Name = "L5"
+        Me.L5.Size = New System.Drawing.Size(0, 13)
+        Me.L5.TabIndex = 11
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.ForeColor = System.Drawing.Color.Red
+        Me.Label1.Location = New System.Drawing.Point(124, 19)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(82, 13)
+        Me.Label1.TabIndex = 12
+        Me.Label1.Text = "ادخل الرقم الاول"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.ForeColor = System.Drawing.Color.Red
+        Me.Label2.Location = New System.Drawing.Point(120, 71)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(86, 13)
+        Me.Label2.TabIndex = 13
+        Me.Label2.Text = "ادخل الرقم الثالث"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.ForeColor = System.Drawing.Color.Red
+        Me.Label3.Location = New System.Drawing.Point(119, 45)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(87, 13)
+        Me.Label3.TabIndex = 14
+        Me.Label3.Text = "ادخل الرقم الثانى"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.ForeColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Label4.Location = New System.Drawing.Point(159, 110)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(15, 13)
+        Me.Label4.TabIndex = 15
+        Me.Label4.Text = "="
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.عبدالله_بدر_الدين_على.My.Resources.Resources._4
+        Me.ClientSize = New System.Drawing.Size(334, 289)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.L5)
+        Me.Controls.Add(Me.L2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.T3)
+        Me.Controls.Add(Me.T2)
+        Me.Controls.Add(Me.T1)
+        Me.Controls.Add(Me.L1)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.ForeColor = System.Drawing.Color.Black
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents L1 As System.Windows.Forms.Label
+    Friend WithEvents T1 As System.Windows.Forms.TextBox
+    Friend WithEvents T2 As System.Windows.Forms.TextBox
+    Friend WithEvents T3 As System.Windows.Forms.TextBox
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents L2 As System.Windows.Forms.Label
+    Friend WithEvents L5 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+
+End Class
---
+++ عبدالله بدر الدين على     1 1 1/عبدالله بدر الدين على     1 1 1/Fo1111.vb
@@ -0,0 +1,45 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        L1.Text = T1.Text * T2.Text * T3.Text
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        If T3.Text = 0 Then L2.Text = "لايمكن القسمه على صفر"
+        If T2.Text = 0 Then L2.Text = "لايمكن القسمه على صفر"
+        L1.Text = T1.Text / T2.Text / T3.Text
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        L1.Text = Val(T1.Text) + Val(T2.Text) + Val(T3.Text)
+    End Sub
+
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+        L1.Text = T1.Text - T2.Text - T3.Text
+    End Sub
+
+    Private Sub Button5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button5.Click
+        MsgBox("!!!!...  نرجو ان نكون قد اسعدناكم مع اطيب تمنياتى ........ عبالله بدر الدين", MsgBoxStyle.Question, "QUESTION")
+        End
+    End Sub
+
+    Private Sub PictureBox1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles PictureBox1.Click
+        
+    End Sub
+
+    Private Sub PictureBox1_MouseLeave(ByVal sender As Object, ByVal e As System.EventArgs) Handles PictureBox1.MouseLeave
+        L5.Text = ""
+        L5.Text = "ABDALLA_BADR_ALDIAN"
+
+    End Sub
+
+    Private Sub PictureBox1_MouseMove(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles PictureBox1.MouseMove
+        L5.Text = ""
+        L5.Text = "ABDALLA_BADR_ALDIAN"
+    End Sub
+
+    Private Sub PictureBox1_Move(ByVal sender As Object, ByVal e As System.EventArgs) Handles PictureBox1.Move
+        L5.Text = ""
+        L5.Text = "ABDALLA_BADR_ALDIAN"
+    End Sub
+End Class
